68e785a7deee893a82056cec6405c3acc6c67d4d
Marcin Nalborczyk <marcin.nalborczyk@ibm.com>
2026-01-16 22:41:45 +0100

    w5 d4 work

--- week5/implementation/ingest.py
+++ week5/implementation/ingest.py
@@ -38,7 +38,7 @@ def fetch_documents():
 
 
 def create_chunks(documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1666, chunk_overlap=200)
     chunks = text_splitter.split_documents(documents)
     return chunks
 
